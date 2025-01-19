fc4d07214493eab9e4205e15c7010d7437c02e77
Diego-glitch-cloud <dcalderon918@gmail.com>
2025-01-18 21:15:53 -0600

    Creación de archivo test y lib para prubeas unitarias

--- Licuadora2.java
+++ Licuadora2.java
@@ -6,34 +6,43 @@ public class Licuadora2 implements Interfaz {
     public Licuadora2() {
         this.encendido = false;
         this.llena = false; 
-        this.velocidad = 0;
+        this.velocidad = 0; // Máxima velocidad 3, mínima 1. 0 = apagada
     }
     
     @Override
     public void aumentarVelocidad() {
-        velocidad +=1;
+        // Para esto debe estar encendida y llena
+        velocidad += 1;
         
     }
+
     @Override
     public int consultarVelocidad() {
         return velocidad;
     }
+
     @Override
     public boolean consultarllenado() {
         return llena;
     }
+
     @Override
     public void encender() {
+        // Debe estar apagada
         encendido = true;
         
     }
+
     @Override
     public void llenar() {
+        // Debe estar vacía
         llena = true;
         
     }
+
     @Override
     public void vaciar() {
+        // Debe estar llena
         this.llena = false; 
         
     }
---
+++ Licuadora2Test.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class Licuadora2Test {
+    @Test
+    // tests ->
+
+    // Test aumento de velocidad cuando velocidad = 3 -> 
+
+    // Test encendido solo si está llena y encendida -> 
+
+    // Test vaciar estando vacía -> return mensaje de está vacía
+
+}
