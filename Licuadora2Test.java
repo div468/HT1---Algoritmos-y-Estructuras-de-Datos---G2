1c1bdeebacae54c31d0c7a9fd7e133cde9f14352
Diego-glitch-cloud <dcalderon918@gmail.com>
2025-01-19 18:22:23 -0600

    unit Test terminados

--- Licuadora2Test.java
+++ Licuadora2Test.java
@@ -2,13 +2,56 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class Licuadora2Test {
+    // Test encendido 
     @Test
-    // tests ->
+    public void testEnceder() {
+        // Inicializar
+        Licuadora2 licuadora = new Licuadora2();
+        licuadora.encender();
 
+        // Test encendido
+        assertTrue(licuadora.getEncendido());
+
+        // Test velocidad inicia con 1
+        assertEquals(1, licuadora.consultarVelocidad());
+    }
+    
     // Test aumento de velocidad cuando velocidad = maximo -> 
+    @Test
+    public void testVelocidad_Maxima() {
+        // Inicializar 
+        Licuadora2 licuadora = new Licuadora2();
+        licuadora.encender();
 
-    // Test encendido solo si está llena y encendida -> 
+        for (int i = 0; i < 10; i++) {
+            // Empieza en velocidad = 1
+            licuadora.aumentarVelocidad();
+        }
+        
+        // Verificamos que regrese a 1 tras superar el límite
+        assertEquals(1, licuadora.consultarVelocidad());
+    }
 
-    // Test vaciar estando vacía -> return mensaje de está vacía
+    // Test apagado con velocidad = 5
+    @Test
+    public void testApagado_velocidad5() {
+        // Inicializar
+        Licuadora2 licuadora = new Licuadora2();
+        licuadora.encender();
 
+        for (int i = 0; i < 4; i++) {
+            licuadora.aumentarVelocidad();
+        }
+        
+        // Test velocidad = 5
+        assertEquals(5, licuadora.consultarVelocidad());
+
+        // apagar licuadora
+        licuadora.apagar();
+
+        // Test de apagado
+        assertFalse(licuadora.getEncendido());
+
+        assertEquals(0, licuadora.consultarVelocidad());
+    }    
 }
